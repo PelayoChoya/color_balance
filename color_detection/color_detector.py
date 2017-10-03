062ef49b0a2530c9797454faa7790530aa6616e5
PelayoChoya <pelayo.choya@gmail.com>
2017-10-03 17:45:08 +0200

    Deleted resizing of the image, dataset's images sizes changed

--- color_detection/color_detector.py
+++ color_detection/color_detector.py
@@ -31,33 +31,24 @@ class ColorDetector:
     def detect_color(self,inImg_dir):
 
         inImg = cv2.imread(inImg_dir)
-        dim = (1280,720)
-        # perform the actual resizing of the image and show it
-        inImg_resized = cv2.resize(inImg, dim, interpolation = cv2.INTER_AREA)
-        # cv2.imshow("resized", inImg_resized)
-        # cv2.waitKey(0)
-        cv2.destroyAllWindows()
 
         # color detection process
-        inImg_filtered = cv2.GaussianBlur(inImg_resized, (5,5),0)
+        inImg_filtered = cv2.GaussianBlur(inImg, (5,5),0)
 
         #convertion from rgb to hsv
         inImg_hsv = cv2.cvtColor(inImg_filtered, cv2.COLOR_BGR2HSV)
 
         #appliying the color filter
         mask = cv2.inRange(inImg_filtered,self.color[1][0], self.color[1][1])
-        # cv2.imshow("mask", mask)
 
         #morphological transformation
         #kernel = np.ones((7,7),np.uint8)
         mask_op = cv2.morphologyEx(mask, cv2.MORPH_OPEN, self.kernel_op)
         mask_op_cl = cv2.morphologyEx(mask_op, cv2.MORPH_CLOSE, self.kernel_cl)
-        #cv2.imshow("mask opening closing", mask_op_cl)
 
         #removing the small objects from the binary image
         contours,h = cv2.findContours(mask_op_cl,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
         mask_final = np.ones(mask_op_cl.shape[:2], dtype="uint8") * 255
-        #cv2.imshow("middle step", mask_final)
         area_ev = 0
         iterator = 0
         biggest_area_index = 0
@@ -73,9 +64,8 @@ class ColorDetector:
             cnt =  contours[biggest_area_index]
             cv2.drawContours(mask_final, [cnt], -1, 0, -1)
             cv2.bitwise_not(mask_final,mask_final)
-            # cv2.imshow("hue", mask_final)
+
             #check if the color filer succeed
             if area_ev > 20000:
                 self.increase_success_number()
                 self.include_possitive_image(inImg_dir)
-            # cv2.waitKey(0)
