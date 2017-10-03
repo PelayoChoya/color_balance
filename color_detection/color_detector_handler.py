45d85d313570d2d07a9e8ad9ef23e5f838281d98
PelayoChoya <pelayo.choya@gmail.com>
2017-10-03 15:30:15 +0200

    created a dictionary containing the results of the test and methods for handling it

--- color_detection/color_detector_handler.py
+++ color_detection/color_detector_handler.py
@@ -8,13 +8,30 @@ class ColorDetectorHandler:
     def __init__(self, path_to_dataset_dir):
         # list containing all the images in the dataset directory
         self.image_dataset = sorted(glob.glob(path_to_dataset_dir + "/*"))
-        self.detectable_colors = {'Red': ColorDetector('Red'),
+        # dictionary that contains the color, the instance of the class, 
+        # the number of times it has been detected and the images where
+        # the detection has been successful
+        self.colors = {'Red': [ColorDetector('Red'), 0,[]],
                                   'Blue':
-                                  ColorDetector('Blue'),
+                                  [ColorDetector('Blue'),0,[]],
                                   'Green':
-                                  ColorDetector('Green')}
+                                  [ColorDetector('Green'),0,[]]}
+
+    def success_per_color_update(self, color, number):
+        self.colors[color][1] = number
+
+    def success_per_color_number(self,color):
+        return self.colors[color][1] 
+
+    def successful_images_per_color_update(self, color, images):
+        self.colors[color][2] = images 
+
+    def successful_images_per_color_number(self,color):
+        return self.colors[color][2] 
+
     def detection_process(self, color):
         for image in self.image_dataset:
-            self.detectable_colors[color].detect_color(image)
-        print self.detectable_colors[color].number_success
-        print self.detectable_colors[color].possitive_images
+            self.colors[color][0].detect_color(image)
+        self.success_per_color_update(color, self.colors[color][0].number_success)
+        self.successful_images_per_color_update(color,
+                                                self.colors[color][0].possitive_images)
